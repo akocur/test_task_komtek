a6649f461ac72d3d371a6eb56d03507d5dc37477
akocur <akocur@yandex.ru>
2021-12-09 19:37:48 +0300

    add model Guide and GuideItem. Configure admin site for them.

--- services/terminology/admin.py
+++ services/terminology/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.db import models
+from services.terminology.models import Guide, GuideItem
 
-# Register your models here.
+@admin.register(Guide)
+class GuideAdmin(admin.ModelAdmin):
+    list_display = ('name', 'short_name', 'version', 'start_date')
+    list_filter = ['start_date']
+    search_fields = ['name', 'short_name', 'version']
+
+@admin.register(GuideItem)
+class GuideItemAdmin(admin.ModelAdmin):
+    list_display = ('code', 'value', 'guide')
--- services/terminology/models.py
+++ services/terminology/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Guide(models.Model):
+    name = models.CharField(max_length=200)
+    short_name = models.CharField(max_length=100, blank=True)
+    description = models.TextField(blank=True)
+    version = models.CharField(max_length=100)
+    start_date = models.DateField()
+
+    class Meta(object):
+        ordering = ['name', 'start_date', 'id']
+        unique_together = ['name', 'version']
+
+    def __str__(self):
+        return f'{self.name}, {self.version}'
+
+
+class GuideItem(models.Model):
+    guide = models.ForeignKey('Guide', on_delete=models.CASCADE)
+    code = models.CharField(max_length=100)
+    value = models.CharField(max_length=200)
+
+    class Meta(object):
+        ordering = ['code', 'guide', 'id']
