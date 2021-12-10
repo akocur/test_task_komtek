36fbd18c767c21fcd1e69ffdd835b0b4f54a30ac
akocur <akocur@yandex.ru>
2021-12-10 17:16:37 +0300

    add getting a list of guides relevant on the specified date

--- services/terminology/views.py
+++ services/terminology/views.py
@@ -1,4 +1,6 @@
-from rest_framework import generics
+import datetime
+
+from rest_framework import generics, serializers
 
 from services.terminology.models import Guide
 from services.terminology.serializers import GuideSerializer
@@ -7,5 +9,18 @@ from services.terminology.serializers import GuideSerializer
 class GuideList(generics.ListAPIView):
     """List of guide."""
 
-    queryset = Guide.objects.all()
     serializer_class = GuideSerializer
+
+    def get_queryset(self):  # noqa: WPS615
+        """Get queryset."""
+        queryset = Guide.objects.all()
+        start_date_lte = self.request.query_params.get('start_date_lte')
+        if start_date_lte:
+            try:
+                date = datetime.date.fromisoformat(start_date_lte)
+            except ValueError:
+                raise serializers.ValidationError(
+                    {'start_date_lte': 'Please enter a valid start_date_lte'},
+                )
+            queryset = Guide.objects.filter(start_date__lte=date)
+        return queryset
