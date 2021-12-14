ededb68c98b81ea7bf0f156e1e37612d37cb5e72
akocur <akocur@yandex.ru>
2021-12-14 11:52:43 +0300

    modify models

--- services/terminology/admin.py
+++ services/terminology/admin.py
@@ -1,19 +1,25 @@
 from django.contrib import admin
 
-from services.terminology.models import Guide, GuideItem
+from services.terminology.models import Guide, GuideItem, GuideVersion
 
 
 @admin.register(Guide)
 class GuideAdmin(admin.ModelAdmin):
     """The class represents the Guide model in the admin interface."""
 
-    list_display = ('name', 'short_name', 'version', 'start_date')
-    list_filter = ['start_date']
-    search_fields = ['name', 'short_name', 'version']
+    list_display = ('name', 'short_name')
+    search_fields = ['name', 'short_name']
+
+
+@admin.register(GuideVersion)
+class GuideVersionAdmin(admin.ModelAdmin):
+    """The class represents the GuideVersion model in the admin interface."""
+
+    list_display = ('guide', 'version', 'start_date')
 
 
 @admin.register(GuideItem)
 class GuideItemAdmin(admin.ModelAdmin):
     """The class represents the GuideItem model in the admin interface."""
 
-    list_display = ('code', 'value', 'guide')
+    list_display = ('code', 'value')
--- services/terminology/models.py
+++ services/terminology/models.py
@@ -8,35 +8,54 @@ class Guide(models.Model):
     name = models.CharField(max_length=200)  # noqa: WPS432
     short_name = models.CharField(max_length=100, blank=True)  # noqa: WPS432
     description = models.TextField(blank=True)
-    version = models.CharField(max_length=100)  # noqa: WPS432
-    start_date = models.DateField()
 
-    @classmethod
-    def current_version(cls, name):
-        """Return current version by name."""
-        return cls.objects.filter(
-            name=name, start_date__lte=timezone.now().date(),
+    @property
+    def current_version(self):
+        """Return current version."""
+        return self.versions.filter(
+            start_date__lte=timezone.now().date(),
         ).order_by('-start_date').first()
 
-    class Meta(object):
-        ordering = ['name', 'start_date', 'id']
-        unique_together = ['name', 'version']
+    def get_guide_items(self, version=None):
+        """Return guide items of the specified version.
+
+        If version is None then return guide items of the current version.
+        """
+        guid_version = self.versions.get(version=version)
+        if version is None:
+            guid_version = self.current_version
+        guide_field = models.Value(
+            self.id, output_field=models.IntegerField(),
+        )
+        return guid_version.guide_items.annotate(guide=guide_field)
 
     def __str__(self):
-        """Display the class as a 'name, version' string instead 'id'."""
-        return f'{self.name}, {self.version}'
+        """Return  instanse representation."""
+        return f'{self.id}, {self.name}'
 
 
 class GuideItem(models.Model):
     """Entity GuideItem."""
 
-    guide = models.ForeignKey(
-        Guide,
-        on_delete=models.CASCADE,
-        related_name='items',
-    )
     code = models.CharField(max_length=100)  # noqa: WPS432
     value = models.CharField(max_length=200)  # noqa: WPS432, WPS110
 
+    def __str__(self):
+        """Return  instanse representation."""
+        return f'{self.id}, {self.value}'
+
+
+class GuideVersion(models.Model):
+    """Guide Versions."""
+
+    guide = models.ForeignKey(Guide, models.CASCADE, related_name='versions')
+    version = models.CharField(max_length=100)  # noqa: WPS432
+    start_date = models.DateField()
+    guide_items = models.ManyToManyField(GuideItem)
+
     class Meta(object):
-        ordering = ['code', 'guide', 'id']
+        unique_together = ['guide', 'version']
+
+    def __str__(self):
+        """Return  instanse representation."""
+        return f'{self.version}, {self.guide}'
