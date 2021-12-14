8aaf144297dd1f2b4d247afafa14b88c54a181b9
akocur <akocur@yandex.ru>
2021-12-14 11:58:20 +0300

    add fill_database.py

---
+++ fill_database.py
@@ -0,0 +1,27 @@
+from services.terminology.models import Guide, GuideVersion, GuideItem
+
+
+def populate_db():
+    specialties = Guide.objects.create(name='specialties')
+    facilities = Guide.objects.create(name='facilities')
+
+    specialties.versions.create(version=1, start_date='2021-01-01')
+    specialties.versions.create(version=2, start_date='2021-06-01')
+    specialties.versions.create(version=3, start_date='2052-06-01')
+
+    facilities.versions.create(version=1, start_date='2021-03-01')
+    facilities.versions.create(version=2, start_date='2021-07-01')
+
+    surgeon = GuideItem.objects.create(code=1, value='surgeon')
+    therapist = GuideItem.objects.create(code=2, value='therapist')
+    otolaryngologist = GuideItem.objects.create(
+        code=3, value='otolaryngologist',
+    )
+    dentist = GuideItem.objects.create(code=4, value='dentist')
+
+    specialties_v1 = specialties.versions.get(version=1)
+    specialties_v2 = specialties.versions.get(version=2)
+    specialties_v1.guide_item.add(surgeon, therapist, otolaryngologist)
+    specialties_v2.guide_item.add(
+        surgeon, therapist, dentist, otolaryngologist,
+    )
