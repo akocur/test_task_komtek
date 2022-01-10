75f4e124375fc58b7613f4e06bcaf1b04ab31687
akocur <akocur@yandex.ru>
2022-01-10 13:34:00 +0300

    add russification for models

--- services/terminology/models.py
+++ services/terminology/models.py
@@ -5,9 +5,18 @@ from django.utils import timezone
 class Guide(models.Model):
     """Entity Guide."""
 
-    name = models.CharField(max_length=200)  # noqa: WPS432
-    short_name = models.CharField(max_length=100, blank=True)  # noqa: WPS432
-    description = models.TextField(blank=True)
+    name = models.CharField(
+        max_length=200, verbose_name='Hаименование',  # noqa: WPS432
+    )
+    short_name = models.CharField(
+        max_length=100,  # noqa: WPS432
+        blank=True,
+        verbose_name='Короткое наименование',
+    )
+    description = models.TextField(blank=True, verbose_name='Описание')
+
+    class Meta(object):
+        verbose_name_plural = 'Справочники'
 
     @property
     def current_version(self):
@@ -44,10 +53,17 @@ class Guide(models.Model):
 class GuideItem(models.Model):
     """Entity GuideItem."""
 
-    code = models.CharField(max_length=100)  # noqa: WPS432
-    value = models.CharField(max_length=200)  # noqa: WPS432, WPS110
+    code = models.CharField(
+        max_length=100,  # noqa: WPS432
+        verbose_name='Код элемента',
+    )
+    value = models.CharField(  # noqa: WPS110
+        max_length=200,  # noqa: WPS432
+        verbose_name='Значение элемента',
+    )
 
     class Meta(object):
+        verbose_name_plural = 'Элементы справочников'
         constraints = [
             models.CheckConstraint(
                 check=~models.Q(code=''), name='non_empty_code',
@@ -65,12 +81,25 @@ class GuideItem(models.Model):
 class GuideVersion(models.Model):
     """Guide Versions."""
 
-    guide = models.ForeignKey(Guide, models.CASCADE, related_name='versions')
-    version = models.CharField(max_length=100)  # noqa: WPS432
-    start_date = models.DateField()
-    guide_items = models.ManyToManyField(GuideItem)
+    guide = models.ForeignKey(
+        Guide,
+        models.CASCADE,
+        related_name='versions',
+        verbose_name='Справочник',
+    )
+    version = models.CharField(
+        max_length=100,  # noqa: WPS432
+        verbose_name='Версия',
+    )
+    start_date = models.DateField(
+        verbose_name='Дата начала действия справочника',
+    )
+    guide_items = models.ManyToManyField(
+        GuideItem, verbose_name='Элементы справочника',
+    )
 
     class Meta(object):
+        verbose_name_plural = 'Версии справочников'
         unique_together = ['guide', 'version']
         constraints = [
             models.CheckConstraint(
